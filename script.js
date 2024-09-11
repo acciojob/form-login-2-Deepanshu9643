811f570c37eff2ba243cbf9d6c970af4a88ea2e7
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-11 14:00:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 document.querySelector('#userForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevents the form from submitting normally
     
-    let firstName = document.querySelector('#firstName').value;
-    let lastName = document.querySelector('#lastName').value;
-    let phoneNumber = document.querySelector('#phoneNumber').value;
-    let email = document.querySelector('#email').value;
+    let firstName = document.querySelector('input[name="firstName"]').value;
+    let lastName = document.querySelector('input[name="lastName"]').value;
+    let phoneNumber = document.querySelector('input[name="phoneNumber"]').value;
+    let email = document.querySelector('input[name="email"]').value;
     
     // Format the alert message
     let message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${email}`;
